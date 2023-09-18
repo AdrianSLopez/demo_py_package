dcc217ad9755035981a2fbf802e15ea764009229
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-09-18 13:50:18 -0700

    Update correct download url link

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'demo_py_package',         # How you named your package folder (MyLib)
   packages = ['demo_py_package'],   # Chose the same as "name"
-  version = '0.1.3',      # Start with a small number and increase it with every change you make
+  version = '0.1.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simple package to learn the process of publishing a python package to PyPI',   # Give a short description about your library
   author = 'Adrian Salgado Lopez',                   # Type in your name
   author_email = 'adrianlopezdev@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/AdrianSLopez/demo_py_package',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/AdrianSLopez/demo_py_package/archive/refs/tags/0.1.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/AdrianSLopez/demo_py_package/archive/refs/tags/0.1.4.tar.gz',    # I explain this later on
   keywords = [],   # Keywords that define your package best
   install_requires=[], # package dependencies
   classifiers=[
