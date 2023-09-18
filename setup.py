80b64d3ee2017a11c551635638dcd58fa1d64e18
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-09-18 13:36:03 -0700

    new init file location

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'demo_py_package',         # How you named your package folder (MyLib)
   packages = ['demo_py_package'],   # Chose the same as "name"
-  version = '0.1.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simple package to learn the process of publishing a python package to PyPI',   # Give a short description about your library
   author = 'Adrian Salgado Lopez',                   # Type in your name
