bc7b4bf6829160e917fca0d7ab18c0629b4cb87d
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-09-25 12:29:16 -0700

    Use README for pypi description.

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
   name = 'demo_py_package',         # How you named your package folder (MyLib)
   packages = ['demo_py_package'],   # Chose the same as "name"
-  version = '0.1.7',      # Start with a small number and increase it with every change you make
+  version = '0.1.8',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simple package to learn the process of publishing a python package to PyPI',   # Give a short description about your library
+  long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
+  long_description_content_type='text/markdown',
   author = 'Adrian Salgado Lopez',                   # Type in your name
   author_email = 'adrianlopezdev@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/AdrianSLopez/demo_py_package',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/AdrianSLopez/demo_py_package/archive/refs/tags/0.1.7.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/AdrianSLopez/demo_py_package/archive/refs/tags/0.1.8.tar.gz',    # I explain this later on
   keywords = [],   # Keywords that define your package best
   install_requires=[], # package dependencies
   classifiers=[
